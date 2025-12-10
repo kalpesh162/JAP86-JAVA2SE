23a518bbda330f755fe8df52bad9a1e02d70d6c3
kalpesh <kalpesh162@gmail.com>
2025-12-10 16:53:50 +0530

    Loop number problem 1

---
+++ SampleProgram/Hello.java
@@ -0,0 +1,24 @@
+class Hello{
+
+	public static void main(String[] abc){
+
+		int num1=22;
+		int num2=11;
+
+		// Arithmatic Operator  +  - * / %
+		int res=num1+num2;
+		System.out.println(res);
+		 res=num1-num2;
+		System.out.println(res);
+
+		res=num1*num2;
+		System.out.println(res);
+
+		res=num1/num2;
+		System.out.println(res);
+
+
+	}
+
+
+}
