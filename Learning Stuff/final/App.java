e952ea1c6e1b9f7fda316e9715e884cd37576338
kalpesh <kalpesh162@gmail.com>
2026-02-11 18:33:52 +0530

    added notes on final and static constuctor

---
+++ Learning Stuff/final/App.java
@@ -0,0 +1,17 @@
+
+class A{
+	//modifier static not allowed here
+	// int x  --> instance Varaible 
+	// if constructor static how we can intialize
+	
+	//error: modifier final not allowed here
+	A(){
+
+	}
+}
+
+class App{
+	public static void main(String[] args) {
+		
+	}
+}
