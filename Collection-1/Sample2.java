95697fb50c251ad1d6e93dfe7ef5e1ca320a912f
kalpesh <kalpesh162@gmail.com>
2026-03-09 14:34:17 +0530

    Added Collectio

---
+++ Collection-1/Sample2.java
@@ -0,0 +1,34 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+public  class Sample2{
+	public static void main(String[] args) {
+		
+		// Generic Collection
+		//Collection<Integer>numbers=new ArrayList<Integer>();
+		//List<Integer>numbers=new ArrayList<Integer>();
+
+		ArrayList<Integer>numbers=new ArrayList<Integer>();
+		numbers.add(11);
+		numbers.add(11);
+		numbers.add(44);
+		numbers.add(33);
+		numbers.add(22);
+
+		// duplicates Object are allow
+		// insertion order maintain
+
+		// numbers.contains(new Integer(44))
+		System.out.println("contains  "+numbers.contains(44));
+
+		// public boolean contains(E e){  E e//Integer i
+
+		//}
+
+		//System.out.println("remove"+numbers.remove(44));
+		System.out.println("remove"+numbers.remove(new Integer(44)));
+		System.out.println(numbers);
+
+
+	}
+}
