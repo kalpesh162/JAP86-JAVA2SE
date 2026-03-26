8b09ad80be163f7aba09e7b87247867fde0b8b15
kalpesh <kalpesh162@gmail.com>
2026-03-26 12:41:55 +0530

    jdbc 3

--- StudentProjDB2/src/com/dao/StudentDao.java
+++ StudentProjDB2/src/com/dao/StudentDao.java
@@ -2,14 +2,16 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.model.Student;
 import com.util.DBUtility;
 
 public class StudentDao {
-
 	public int addStudent(Student student) {
 		int noOfRows = 0;
 		Connection connection = DBUtility.getDBConnection();
@@ -17,11 +19,9 @@ public class StudentDao {
 		PreparedStatement ps = null;
 		try {
 			ps = connection.prepareStatement(sql);
-
 			ps.setInt(1, student.getId());
 			ps.setString(2, student.getName());
 			ps.setDouble(3, student.getMarks());
-
 			noOfRows = ps.executeUpdate();
 		} catch (Exception e) {
 			System.out.println("StudentDAO " + e);
@@ -29,7 +29,6 @@ public class StudentDao {
 			try {
 				ps.close();
 			} catch (SQLException e) {
-
 				e.printStackTrace();
 			}
 		}
@@ -37,23 +36,91 @@ public class StudentDao {
 	}
 
 	public int updateStudent(Student student) {
+		int noOfRows = 0;
+		Connection connection = DBUtility.getDBConnection();
+		String sql = "update student set name = ?, marks = ? where id = ?";
+		PreparedStatement ps = null;
 
-		return 0;
+		try {
+			ps = connection.prepareStatement(sql);
+
+			ps.setString(1, student.getName());
+			ps.setDouble(2, student.getMarks());
+			ps.setInt(3, student.getId());
+
+			noOfRows = ps.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("StudentDAO " + e);
+		} finally {
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return noOfRows;
 	}
 
 	public int deleteStudent(int id) {
+		int noOfRows = 0;
+		Connection connection = DBUtility.getDBConnection();
+		String sql = "delete from student where id = ?";
+		PreparedStatement ps = null;
 
-		return 0;
+		try {
+			ps = connection.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			noOfRows = ps.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("StudentDAO " + e);
+		} finally {
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return noOfRows;
 	}
 
 	public int deleteStudent(Student student) {
-
-		return 0;
+		return deleteStudent(student.getId());
 	}
 
 	public List<Student> getAllStudents() {
+		List<Student> students = new ArrayList<>();
+		Connection connection = DBUtility.getDBConnection();
+		String sql = "select * from student";
+		Statement stmt = null;
+		ResultSet rs = null;
 
-		return null;
+		try {
+			stmt = connection.createStatement();
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				Student student = new Student();
+				student.setId(rs.getInt("id"));
+				student.setName(rs.getString("name"));
+				student.setMarks(rs.getDouble("marks"));
+
+				students.add(student);
+			}
+		} catch (Exception e) {
+			System.out.println("StudentDAO " + e);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return students;
 	}
-
 }
