67163c8465eb8ed66af8aa67bba98f52d41e96dc
kalpesh <kalpesh162@gmail.com>
2026-04-01 13:59:01 +0530

    Student Proj 6

--- StudentProj6/src/com/app/App.java
+++ StudentProj6/src/com/app/App.java
@@ -8,15 +8,27 @@ import com.service.StudentService;
 import com.service.StudentServiceImpl;
 
 public class App {
+	private final static String RESET = "\u001B[0m";
+	private final static String GREEN = "\u001B[32m";
+	private final static String YELLOW = "\u001B[33m";
+	private final static String BLUE = "\u001B[34m";
+	private final static String RED = "\u001B[35m";
 
 	public static void printStudents(List<Student> students) {
+
 		Iterator<Student> itr = students.iterator();
-		System.out.println("     ID      NAME          MARKS");
+		System.out.println(GREEN + "     ID      NAME          MARKS" + RESET);
 		while (itr.hasNext()) {
 			Student student = itr.next();
+			System.out.print(BLUE);
 			System.out.printf("%7d |", student.getId());
+			System.out.print(RESET);
+			System.out.print(YELLOW);
 			System.out.printf("%15s|", student.getName());
+			System.out.print(RESET);
+			System.out.print(RED);
 			System.out.printf("%7.2f |", student.getMarks());
+			System.out.print(RESET);
 			System.out.println();
 
 		}
@@ -30,10 +42,10 @@ public class App {
 		// App Layer --> Service --> DAO --> mysql
 
 		StudentService studentService = new StudentServiceImpl();
-		 studentService.registerStudent(student);
+		// studentService.registerStudent(student);
 
 		System.out.println("-------");
-		
+
 		printStudents(studentService.getAllStudents());
 	}
 
--- StudentProj6/src/com/util/DBUtility.java
+++ StudentProj6/src/com/util/DBUtility.java
@@ -1,13 +1,10 @@
 package com.util;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.util.UUID;
 
 public class DBUtility {
 
