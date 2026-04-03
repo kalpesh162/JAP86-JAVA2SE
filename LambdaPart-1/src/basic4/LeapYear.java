9ad9810c6a423f82ecd430623f512ca095e23c55
kalpesh <kalpesh162@gmail.com>
2026-04-03 14:43:11 +0530

    Lambada with return

---
+++ LambdaPart-1/src/basic4/LeapYear.java
@@ -0,0 +1,27 @@
+package basic4;
+//Check if a year is a leap year.
+
+/*
+public interface java.util.function.Predicate<T>{
+	   
+	  boolean test(T t);
+	
+}
+*/
+
+import java.util.function.Predicate;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+
+		Predicate<Integer> isALeapYear = (Integer i) -> (i % 400 == 0 || i % 100 != 0 && i % 4 == 0) ? true : false;
+
+		Predicate<Integer> isALeapYear1 = i -> (i % 400 == 0 || i % 100 != 0 && i % 4 == 0) ? true : false;
+
+		System.out.println(isALeapYear.test(200));
+		System.out.println(isALeapYear.test(1992));
+
+	}
+
+}
