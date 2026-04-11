78e7aca9d61018b7c3aaadaca908bcbca7d67ad2
kalpesh <kalpesh162@gmail.com>
2026-04-11 08:09:40 +0530

    selection sort

---
+++ SortingTechnique/src/com/select/App.java
@@ -0,0 +1,61 @@
+package com.select;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+	private static void printArray(int[] arr) {
+		System.out.println("**** Printing Array **** ");
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(i + "  " + arr[i]);
+
+	}
+
+	private static void inputArray(int[] arr) {
+		Random random = new Random();
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = random.nextInt();
+
+	}
+
+	// Time Complexity n*n
+	public static void selectionSort(int A[]) {
+		for (int i = 0; i < A.length - 1; i++) { //
+			int minIndex = i;
+
+			for (int j = i + 1; j < A.length; j++) {
+				if (A[minIndex] > A[j])
+					minIndex = j;
+			}
+			// SWAP
+			int temp = A[i];
+			A[i] = A[minIndex];
+			A[minIndex] = temp;
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int size;
+		System.out.println("Enter Size of an Array ");
+		size = scanner.nextInt();
+
+		int arr[] = new int[size];
+		inputArray(arr); // Array is filled with numbers
+
+		printArray(arr);
+
+		long sTime = System.currentTimeMillis();
+
+		selectionSort(arr);
+		long eTime = System.currentTimeMillis();
+
+		printArray(arr);
+
+		System.out.println("Time Required To Sort  " + (eTime - sTime) + "  ms");
+
+	}
+
+}
